bd1e9f56ce930a68aa9628e63d15dfe569b85d58
ATM <cs22918980x@gmail.com>
2018-04-30 22:51:27 +0800

    namefail

---
+++ mouseClick/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mouseClick
+{
+    public partial class mouseClick : Form
+    {
+        double time  = 0;
+        public mouseClick()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            time = 0;
+            timeLabel.Text = "You have clicked " + time + " time";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            time++;
+            timeLabel.Text = "You have clicked " + time + " time";
+            
+        }
+    }
+}
